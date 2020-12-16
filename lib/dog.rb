d07a620accad8d3ba30f0cd89cfcdc5cb11c0b5a
bambi teague <teeegsa@gmail.com>
2020-12-16 17:23:37 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,19 @@
 class Dog
-    
-    def initialize
-        @name = name
-      end
-    
-      def name
-        @name
-      end
-    
-      def name=(new_name)
-        @name = new_name
-      end
 
-      def initialize
-        @breed = breed
-      end
-    
-      def breed
-        @breed
-      end
-    
-      def breed=(new_breed)
-        @breed = new_breed
-      end
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+  
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,26 +1,19 @@
 class Person
-    
-    def initialize
-        @name = name
-      end
-    
-      def name
-        @name
-      end
-    
-      def name=(new_name)
-        @name = new_name
-      end
 
-      def initialize
-        @job = job
-      end
-    
-      def job
-        @job
-      end
-    
-      def job=(new_job)
-        @job = new_job
-      end
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+  
 end
